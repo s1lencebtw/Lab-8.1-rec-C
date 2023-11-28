2a5fc5bcccc4fb431c8f8c381e7defa9bb0c9eb5
s1lencebtw <Vitalii.Letnianchyn.RI.2023@lpnu.ua>
2023-11-28 20:23:51 +0200

    Add project files.

---
+++ Lab 8.1 rec C/Lab 8.1 rec C.cpp
@@ -0,0 +1,56 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int Count(char* str, int i, int k)
+{
+	if (strlen(str) < 5)
+		return -1;
+
+	if (i < strlen(str)) {
+		if (str[i] == 'B' || str[i] == 'A' || str[i] == 'S' || str[i] == 'I' || str[i] == 'C') {
+			k++;
+		}
+		return Count(str, i + 1, k);
+	}
+
+	return k;
+}
+
+char* Change(char* dest, const char* str, char* t, int i)
+{
+	if (i < strlen(str) != 0)
+	{
+		if (strncmp(str + i, "BASIC", 5) == 0)
+		{
+			strcat(t, "DELPHI");
+			return Change(dest, str, t += 6, i += 5);
+		}
+		else
+		{
+			*t++ = str[i++];
+			*t = '\0';
+			return Change(dest, str, t, i);
+		}
+	}
+	else
+	{
+		*t++ = str[i++];
+		*t++ = str[i++];
+		*t = '\0';
+		return dest;
+	}
+}
+int main()
+{
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+	cout << "Number of chars BASIC:    " << Count(str, 0, 0) << endl;
+	char* dest1 = new char[151];
+	dest1[0] = '\0';
+	char* dest2;
+	dest2 = Change(dest1, str, dest1, 0);
+	cout << "Modified string (param) : " << dest1 << endl;
+	cout << "Modified string (result): " << dest2 << endl;
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 8.1 rec C/Lab 8.1 rec C.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+    TEST_CLASS(UnitTest)
+    {
+    public:
+
+        TEST_METHOD(FindCharTest)
+        {
+            char str[12] = "BASIC";
+            int result = Count(str, 1, 0);
+            Assert::AreEqual(result, 4);
+        }
+    };
+}
